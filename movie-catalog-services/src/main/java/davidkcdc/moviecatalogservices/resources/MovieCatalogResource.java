6ed20a3f27feafd7fcab71d2b14be59d3ada0b49
David_K <david.kristiawan@mitrais.com>
2020-04-15 17:17:45 +0800

    First Commit - video 13

---
+++ movie-catalog-services/src/main/java/davidkcdc/moviecatalogservices/resources/MovieCatalogResource.java
@@ -0,0 +1,50 @@
+package davidkcdc.moviecatalogservices.resources;
+
+import davidkcdc.moviecatalogservices.models.CatalogItem;
+import davidkcdc.moviecatalogservices.models.Movie;
+import davidkcdc.moviecatalogservices.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog (@PathVariable("userId") String userId){
+
+
+        List <Rating> ratings = Arrays.asList(
+                new Rating("1234", 1),
+                new Rating("2345", 2)
+        );
+
+        return ratings.stream().map(rating -> {
+                //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                Movie movie = webClientBuilder.build()
+                        .get()
+                        .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                        .retrieve()
+                        .bodyToMono(Movie.class)
+                        .block();
+
+                return new CatalogItem( movie.getName(), "film Transformer", rating.getRating());
+            })
+        .collect(Collectors.toList());
+    }
+}
---
+++ ratings-data-services/src/test/java/davidkcdc/ratingsdataservices/RatingsDataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package davidkcdc.ratingsdataservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
